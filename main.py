5bc546ed32d5b314fede6a0a8ad0c47988364d93
Aditya Shah <59452496+TangyTiger@users.noreply.github.com>
2023-02-23 12:20:32 -0800

    progress made, input of spike height returns position

--- main.py
+++ main.py
@@ -1,19 +1,32 @@
 mole = 602200000000000000000000
+k = 1000
+
 
 def main():
-    sum = 0
-    sum += even()
-    sum += odd()
-    print(sum)
+  sum = 0
+  #sum += even()
+  sum += odd()
+  print(sum)
+  #print(f"sum: {sum}")
+
 
 def even():
-    return (mole/2)
+  return (mole / 2)
+
+
+def magic(x):
+  #given a spike value, it will return the position that happens in
+  return 2**x - 1
+
 
 def odd():
-    # so there is a pattern where for odd numbers, there is a spike after a set of numbers.
-    # for example
-    return 0 
+  
+  # 2^x - 1 is magic equation
+
+  # so there is a pattern where for odd numbers, there is a spike after 2x+1 where x is prev number.
+  # spike at 3, 7, 15, 31, 63, 127, 255, 511, 1023, 2047
+  return magic(k)
 
 
 if __name__ == "__main__":
-    main()
+  main()
