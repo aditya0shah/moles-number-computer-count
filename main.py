c53bc539216cfd830da5cd1f419e3db397157ea6
Aditya Shah <shahaditya@outlook.com>
2023-02-22 16:50:16 -0800

    time for math

--- main.py
+++ main.py
@@ -3,10 +3,16 @@ mole = 602200000000000000000000
 def main():
     sum = 0
     sum += even()
+    sum += odd()
     print(sum)
 
 def even():
     return (mole/2)
+
+def odd():
+    # so there is a pattern where for odd numbers, there is a spike after a set of numbers.
+    # for example
+    return 0 
 
 
 if __name__ == "__main__":
