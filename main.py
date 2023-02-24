64a0d6c666836fb11e23c14dec3031baccfe09e5
arvickc <arvick@outlook.com>
2023-02-23 20:19:57 -0800

    binary operations for any given decimal number

--- main.py
+++ main.py
@@ -1,38 +1,17 @@
 import math
 
+#mole = 602200000000000000000000
+total = 79 # Total amount of new spikes
+mghz = 100
+speed = mghz*100000000
+#spike_list = []
 
-mole = 602200000000000000000000
-total = 79
-spike_list = []
-
-
-#There are 79 unique peaks in the number
 def main():
-  print(function(778578))
-  # sum = 0
-  # sum += even()
-  # sum += peaks()
+  print(decimalToBinaryOperations(33319))
+
+def decimalToBinaryOperations(x):
+  if(x%2==0): return 1
   
-  print(sum)
-  print(mole)
-  #print(f"sum: {sum}")
-
-
-def spike_list():
-    for i in range(79):
-        spike_list.append(2**i-1)
-    
-
-
-
-    
-
-
-def even():
-  return (mole / 2)
-
-def function(x):
-  spikes = "232 4 232 5 2324232 6 232423252324232"
   y = magic_decimal(x)
   if(y != int(y)):
     # Has decimal (Not a new spike)
@@ -41,15 +20,19 @@ def function(x):
     dif = x - x2
     if(dif%4 == 0):
       # Is a spike
+      done = False
       dif = dif/4
       l = math.log(dif, 2)
-      if(l == int(l)):
-        return l+2
-      else:
-        for a in range(1, int(l)+1):
-          if(2^a > l): break
-          if(l%(2^a) != 0):
-            return a+1
+        
+      while not done:
+        if(l == int(l)):
+          done = True
+          break
+        dif = dif - 2**(int(l))
+        l = math.log(dif, 2)
+      
+      l=math.log(dif, 2)
+      return l+2
     else:
       # Not a spike
       return 1
@@ -64,13 +47,6 @@ def magic_spike(x):
 def magic_decimal(x):
   return (math.log(x+1) / math.log(2))
 
-#def odd():
-  
-  # 2^x - 1 is magic equation
-
-  # so there is a pattern where for odd numbers, there is a spike after 2x+1 where x is prev number.
-  # spike at 3, 7, 15, 31, 63, 127, 255, 511, 1023, 2047
-  #return magic(k)
 
 
 if __name__ == "__main__":
