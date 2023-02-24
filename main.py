fe4973049c631dc3cd00a42b31a709c63f2b6a29
Aditya Shah <shahaditya@outlook.com>
2023-02-23 19:00:37 -0800

    kinda on a brain storm

--- main.py
+++ main.py
@@ -1,38 +1,70 @@
+import math
+
+
 mole = 602200000000000000000000
-k = 79
+total = 79
+spike_list = []
 
 
 #There are 79 unique peaks in the number
 def main():
-  sum = 0
-  sum += even()
-  sum += peaks()
+  print(function(778578))
+  # sum = 0
+  # sum += even()
+  # sum += peaks()
   
   print(sum)
   print(mole)
   #print(f"sum: {sum}")
 
 
-def peaks():
-  temp = 0
-  for i in range(79):
-    temp+=i
-  return temp
+def spike_list():
+    for i in range(79):
+        spike_list.append(2**i-1)
+    
+
+
+
     
 
 
 def even():
   return (mole / 2)
 
+def function(x):
+  spikes = "232 4 232 5 2324232 6 232423252324232"
+  y = magic_decimal(x)
+  if(y != int(y)):
+    # Has decimal (Not a new spike)
+    y = int(y)
+    x2 = magic_spike(y)
+    dif = x - x2
+    if(dif%4 == 0):
+      # Is a spike
+      dif = dif/4
+      l = math.log(dif, 2)
+      if(l == int(l)):
+        return l+2
+      else:
+        for a in range(1, int(l)+1):
+          if(2^a > l): break
+          if(l%(2^a) != 0):
+            return a+1
+    else:
+      # Not a spike
+      return 1
+  else:
+    # No decimal
+    return y
 
 def magic_spike(x):
   #given a spike value, it will return the position that happens in
   return 2**x - 1
 
 def magic_decimal(x):
-  return ((math.log(x-1) / math.log(2))
+  return (math.log(x+1) / math.log(2))
 
-def odd():
+#def odd():
   
   # 2^x - 1 is magic equation
 
@@ -43,3 +75,4 @@ def odd():
 
 if __name__ == "__main__":
   main()
+
